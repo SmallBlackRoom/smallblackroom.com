9eae6e0c6e9450014e9a8be70b88a6e0d3a62bfc
Hugo Ferreira <hugo@mindclick.info>
2014-05-02 03:38:07 +0100

    Facebook: Toggle buttons using jQuery

---
+++ Facebook_LSP/tab_beatrix_potter_colecao/js/functions.js
@@ -0,0 +1,27 @@
+$(document).ready(function() {
+    
+    $('#toggle-livros').click(function(){
+        $('#livros').show();
+        buttonOn('#toggle-livros');
+        
+        $('#bio').hide();
+        buttonOff('#toggle-bio');
+    });
+    
+    $('#toggle-bio').click(function(){
+        $('#livros').hide();
+        buttonOff('#toggle-livros');
+        
+        $('#bio').show();
+        buttonOn('#toggle-bio');
+    });
+
+});
+
+function buttonOn(id) {
+    $(id + ' img:first-child').attr("src").replace("_off", "_on");
+}
+
+function buttonOff(image) {
+    $(id + ' img:first-child').attr("src").replace("_on", "_off");
+}
